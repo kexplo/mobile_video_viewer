d583b3a2c6d7d47bda8e61780f783c228032ed26
Chanwoong Kim <devk0817@gmail.com>
2014-10-06 02:37:53 +0900

    flask 적용.
    flask에서 파일 목록을 제공하도록..
    static 파일들 경로 flask에 맞게 이동.

---
+++ run.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+from flask import Flask, render_template, url_for, send_from_directory
+app = Flask(__name__)
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+VIDEO_DIR = u'./video'
+
+@app.route('/')
+def index():
+    fileList = [f for f in os.listdir(VIDEO_DIR) if os.path.isfile(os.path.join(VIDEO_DIR,f))]
+    return render_template('index.html', fileList=fileList)
+
+@app.route('/v/<string:file_name>')
+def get_video(file_name):
+    return send_from_directory('./video', file_name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
